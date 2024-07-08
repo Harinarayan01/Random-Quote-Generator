36330251ea54d7f65ed24b238c2c4cf251ebbe94
Hari Narayan <harinarayan947255@gmail.com>
2024-07-09 00:07:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import axios from 'axios';
+import './App.css'
+function App() {
+  const [authoy ,setouthy]=useState("");
+  const [quot ,setquot]=useState("");
+  const handleclick=()=>{
+   axios.get('https://api.quotable.io/random').then((res)=>{
+    // console.log(res);
+    setouthy(res.data.author);
+    setquot(res.data.content);
+   })
+  }
+  return (
+    <>
+    <h1>Random Quote Generator</h1>
+    <div className='quote-box'>
+      <div className='quote-text'>
+        <p>{quot}
+        </p>
+      </div>
+      <div className='quote-author'>
+        <h3> {authoy}</h3>
+      </div>
+      <div className='quote-button'>
+        <button onClick={handleclick}>Button </button>
+      </div>
+
+    </div>
+    </>
+  )
+}
+
+export default App
